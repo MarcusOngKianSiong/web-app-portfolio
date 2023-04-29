f8de15cc8603c1118d807ca1c7a386b8d26a9088
Marcus <marcusongkiansiong@gmail.com>
2023-04-29 23:09:43 +0800

    same

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
       element: <App/>,
     },
     {
-      path: "/*/documentation",
+      path: "/web-app-portfolio/documentation",
       element: <Documentation/>
     }
 ]);
