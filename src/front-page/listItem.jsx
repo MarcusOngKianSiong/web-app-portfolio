18cdf96ca6b460347518fc3c73e1a5833b5bc108
Marcus <marcusongkiansiong@gmail.com>
2023-04-29 23:03:52 +0800

    changing links

--- src/front-page/listItem.jsx
+++ src/front-page/listItem.jsx
@@ -6,7 +6,8 @@ export default function ListItem(prop){
     return (
         <div className="d-flex justify-content-around border-bottom border-dark m-4" style={{width: '50%'}}>
             <p>{prop.projectName}</p>        
-            <a id={prop.project_id} href={`${baseUrl}/documentation?projectID=${prop.project_id}&title=${prop.projectName}`}>Documentation</a>
+            {/* <a id={prop.project_id} href={`${baseUrl}/documentation?projectID=${prop.project_id}&title=${prop.projectName}`}>Documentation</a> */}
+            <a id={prop.project_id} href={`https://marcusongkiansiong.github.io/web-app-portfolio/documentation?projectID=${prop.project_id}&title=${prop.projectName}`}>Documentation</a>
         </div>
     )
 }
